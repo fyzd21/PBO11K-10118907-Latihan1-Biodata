a4d84f7958aff95eda220dafe4f6faf1544e450e
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:07:03 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan1/biodata/PBO11K10118907Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan menampilkan value ke layar
+ * 
+ */
+public class PBO11K10118907Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------------");
+        System.out.println("NAMA\t\t: Rizki Adam Kurniawan");
+        System.out.println("ALAMAT\t\t: Jalan Antapani no 89");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Januari 1991");
+        System.out.println("-------------------");
+        System.out.println("HONI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
